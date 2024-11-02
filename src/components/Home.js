1fafd5ac797fac3baf6f766ceca8b455e3700498
iodoku <jdhi0112@naver.com>
2024-11-02 18:32:13 +0900

    test

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <h1>홈 페이지</h1>;
+};
+
+export default Home;
---
+++ src/components/Trending.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Trending = () => {
+  return <h1>대세 콘텐츠 페이지</h1>;
+};
+
+export default Trending;
