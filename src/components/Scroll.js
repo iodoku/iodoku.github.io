e8d352c536e5e996a2fdbae4d9bdbd1002603ac9
iodoku <jdhi0112@naver.com>
2024-11-05 00:52:17 +0900

    betterscroll

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,37 +1,12 @@
-import React, { useRef, useEffect } from 'react';
 import './Scroll.css';
+import { useCallback } from 'react';
 
 export const useHorizontalScroll = () => {
-    const ref = useRef();
+  const onWheel = useCallback((e) => {
+    const scrollContainer = e.currentTarget;
+    scrollContainer.scrollLeft += e.deltaY*25; // 수직 휠 이동에 따라 수평 스크롤
+    e.preventDefault(); // 기본 스크롤 동작 방지
+  }, []);
 
-    useEffect(() => {
-        const element = ref.current;
-
-        const onWheel = (event) => {
-            if (event.deltaY !== 0) {
-                event.preventDefault();
-                element.scrollBy({
-                    left: event.deltaY * 20, // 스크롤 속도 조정
-                    behavior: 'smooth',
-                });
-
-                // 현재 스크롤 위치 출력
-                console.log('Current scroll position:', element.scrollLeft);
-            }
-        };
-
-        // ref가 설정된 후에 이벤트 리스너 추가
-        if (element) {
-            element.addEventListener('wheel', onWheel);
-        }
-
-        // 클린업 함수
-        return () => {
-            if (element) {
-                element.removeEventListener('wheel', onWheel);
-            }
-        };
-    }, [ref]); // ref를 의존성 배열에 추가
-
-    return ref;
+  return { onWheel }; // onWheel을 반환
 };
