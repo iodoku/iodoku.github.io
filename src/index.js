55cb7a9e535d8b554044aae41d965756c1dcfa65
iodoku <jdhi0112@naver.com>
2024-12-22 02:36:55 +0900

    Deploy to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -3,24 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 if (window.Kakao && !window.Kakao.isInitialized()) {
   window.Kakao.init(process.env.REACT_APP_KAKAO_REST_API_KEY);
   console.log('✅ Kakao SDK Initialized:', window.Kakao.isInitialized());
 }
 
-// 환경에 따라 basename 설정
-const basename =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_BASE_URL.replace(window.location.origin, '') || ''
-    : '';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={basename}>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
 
 reportWebVitals();
