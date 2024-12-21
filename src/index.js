4ada8a276b5a0dd0f82a6296677c28197c1712ca
iodoku <jdhi0112@naver.com>
2024-12-22 00:24:20 +0900

    Deploy to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+if (window.Kakao && !window.Kakao.isInitialized()) {
+  window.Kakao.init(process.env.REACT_APP_KAKAO_REST_API_KEY);
+  console.log('✅ Kakao SDK Initialized:', window.Kakao.isInitialized());
+}
+
+// 명확한 basename 설정
+const basename =
+  process.env.NODE_ENV === 'production'
+    ? '/iodoku.github.io'
+    : '';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.REACT_APP_API_BASE_URL || '/'}>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
