d649ee9cd2db3148fc4456dc312d3f2941b08970
iodoku <jdhi0112@naver.com>
2024-12-21 23:05:22 +0900

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React,{ useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header'; 
-import Main from './components/Main'; // 홈 컴포넌트
-import Popular from './components/popular'; // 대세 콘텐츠 컴포넌트
-import Wishlist from './components/Wishlist'; // 내가 찜한 리스트 컴포넌트
-import Search from './components/search'; // 찾아보기 컴포넌트
-import Sign from './components/Sign'; // 내가 찜한 리스트 컴포넌트
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Main from './components/Main';
+import Popular from './components/popular';
+import Wishlist from './components/Wishlist';
+import Search from './components/search';
+import Sign from './components/Sign';
 import KakaoInfo from './components/kakaoinfo';
 
 function App() {
-
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
@@ -20,9 +19,9 @@ function App() {
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/sign" element={<Sign />} />
         <Route path="/kakaoinfo" element={<KakaoInfo />} />
-        {/* 다른 라우트 추가 */}
+        {/* 추가 라우트 */}
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-
 if (window.Kakao && !window.Kakao.isInitialized()) {
   window.Kakao.init(process.env.REACT_APP_KAKAO_REST_API_KEY);
   console.log('✅ Kakao SDK Initialized:', window.Kakao.isInitialized());
@@ -18,7 +17,4 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
