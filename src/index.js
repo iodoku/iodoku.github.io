5ed641a0916c92522fa273cb0ef1b32287aad98d
iodoku <jdhi0112@naver.com>
2024-12-22 18:52:21 +0900

    Deploy to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ if (window.Kakao && !window.Kakao.isInitialized()) {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.REACT_APP_API_BASE_URL}>
     <App />
   </BrowserRouter>
 );
